8c14713262c62d84522450128cfa20b48ff12029
Jennifer Ramirez <jenramirez5@hotmail.com>
2018-10-03 20:43:09 -0700

    demoed javascript options

---
+++ index.js
@@ -0,0 +1,8 @@
+var countTo = prompt("how high should we count even numbers?")
+
+// (init: condition; post-loop)
+for(var i = 0; i < 10; i++) {
+    if(i % 2 === 0) {
+        console.log (i, " is even");
+    }
+}
